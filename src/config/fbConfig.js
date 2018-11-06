c56f3fd23d1a4edd216eab0c6649959e8fa83780
jhemyson sousa <jhemyson_@hotmail.com>
2018-11-06 13:02:04 -0200

    add firebase functions cli

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,12 +4,12 @@ import 'firebase/auth'
 
 // Replace this with your own config details
 var config = {
-  apiKey: "AIzaSyBfSkZDU8MH8Kmp37fgxrYbQb36Srsqcsk",
-  authDomain: "teste-react-firebase.firebaseapp.com",
-  databaseURL: "https://teste-react-firebase.firebaseio.com",
-  projectId: "teste-react-firebase",
-  storageBucket: "teste-react-firebase.appspot.com",
-  messagingSenderId: "631369521673"
+  apiKey: "AIzaSyAm0nJg4IzyYNJ8kqiko8fFiQ6tdfCtP4k",
+  authDomain: "teste-react-firebase-ab588.firebaseapp.com",
+  databaseURL: "https://teste-react-firebase-ab588.firebaseio.com",
+  projectId: "teste-react-firebase-ab588",
+  storageBucket: "teste-react-firebase-ab588.appspot.com",
+  messagingSenderId: "702255608441"
 };
 firebase.initializeApp(config);
 firebase.firestore().settings({ timestampsInSnapshots: true })
